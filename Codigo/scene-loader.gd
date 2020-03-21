974d993ccb442b37ac6466ff7e39d233c7063e3b
Gabriel <gabrielcancro@gmail.com>
2020-03-21 14:52:16 -0300

    scene-loader

---
+++ Codigo/scene-loader.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var scenes_loaded={}
+var last_scene
+
+func _ready():
+	load_scene("Cocina")
+	#load_scene("Patio")
+	#load_scene("Calle")
+	change_scene_to("Cocina")
+
+func load_scene(scene_name):
+	var SCN=load("res://Escenas/Lugares/"+scene_name+".tscn")
+	scenes_loaded[scene_name]=SCN.instance()
+
+func change_scene_to(scene_name):
+	if last_scene: remove_child(last_scene)
+	add_child(scenes_loaded[scene_name])
+	last_scene=scenes_loaded[scene_name]
