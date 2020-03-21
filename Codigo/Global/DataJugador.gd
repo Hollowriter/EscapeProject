4d8ce800c92c48ecb822d727625ee4df0cc1a93b
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-03-21 18:36:27 -0300

    probando parsear

--- Codigo/Global/DataJugador.gd
+++ Codigo/Global/DataJugador.gd
@@ -12,7 +12,7 @@ func cargar_datos():
 	if jugadorData == null:
 		var dict = {"inventario":{}}
 		for slot in range (0, maxSlots):
-			dict["inventario"][String(slot)] = {"id": "0", "amount": 0}
+			dict["inventario"][String(slot)] = {"id": "0", "nombre": "cosita"}
 		DataParser.escribir_datos(url_JugadorData, dict)
 		inventario = dict["inventario"]
 		print("Llegue a parsear")
--- Codigo/Inventario.gd
+++ Codigo/Inventario.gd
@@ -9,5 +9,20 @@ func _ready():
 	itemList.set_select_mode(itemList.SELECT_SINGLE)
 	itemList.set_same_column_width(true)
 	
-	var icon = ResourceLoader.load("res://Visuales/piedrita.png")
-	itemList.add_item("", icon, true)
+	#var icon = ResourceLoader.load("res://Visuales/piedrita.png")
+	#itemList.add_item("", icon, true)
+	cargar_items()
+
+func cargar_items():
+	itemList.clear();
+	for slot in range(0, DataJugador.maxSlots):
+		var itemDeInventario = DataJugador.inventario[String(slot)]
+		var metaDataDelItem = DataItems.get_item(itemDeInventario["id"])
+		var nombre = String(metaDataDelItem["nombre"])
+		var icon = ResourceLoader.Load(metaDataDelItem["icon"])
+		if itemDeInventario["id"] == "0":
+			nombre = " "
+		itemList.add_item(String(nombre), icon, true)
+		itemList.set_item_metadata(slot, metaDataDelItem)
+		itemList.set_item_tooltip(slot, metaDataDelItem["nombre"])
+		
