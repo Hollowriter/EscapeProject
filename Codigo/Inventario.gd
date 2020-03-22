19318e1bc7240f55161df4400fe32a55a66458f7
Manpacman <tadeojliuzzi@gmail.com>
2020-03-22 11:29:40 -0300

    Leer descripcion en inventario

---
+++ Codigo/Inventario.gd
@@ -0,0 +1,52 @@
+extends Control
+
+var inventarioVisible = false
+
+onready var animationPlayer = $AnimationPlayer
+onready var escenaPrin = get_parent().get_parent()
+onready var cajaDeTexto = get_parent().get_node("CajaDeTexto")
+
+func BotonInventarioApretado():
+	if animationPlayer.is_playing():
+		return
+	
+	if inventarioVisible:
+		animationPlayer.play_backwards("Mostrar")
+	else:
+		animationPlayer.play("Mostrar")
+	
+	inventarioVisible = !inventarioVisible
+
+func AgregarItem(lugar, imagen, escala=1):
+	var item = get_node("BotonInventario/Espacios/Espacio" + str(lugar) + "/Item")
+	item.texture = imagen
+	item.rect_scale = Vector2(escala,escala)
+
+
+func LeerDescripcion(numero):
+	pass
+
+func SeleccionarItem(numero):
+	pass
+
+func MostrarDescripcion(numero):
+	if escenaPrin.contenido[numero].size() > 1:
+		cajaDeTexto.NuevoTexto(escenaPrin.contenido[numero][1])
+
+func MousePasoPorArriba0():
+	MostrarDescripcion(0)
+
+func MousePasoPorArriba1():
+	MostrarDescripcion(1)
+
+func MousePasoPorArriba2():
+	MostrarDescripcion(2)
+
+func MousePasoPorArriba3():
+	MostrarDescripcion(3)
+
+func MousePasoPorArriba4():
+	MostrarDescripcion(4)
+
+func MousePasoPorArriba5():
+	MostrarDescripcion(5)
--- Codigo/scene-loader.gd
+++ Codigo/scene-loader.gd
@@ -1,5 +1,10 @@
 extends Node
 
+<<<<<<< Updated upstream
+=======
+var espacios = ["","","","","",""]
+var contenido = [[],[],[],[],[],[]]
+>>>>>>> Stashed changes
 var scenes_loaded={}
 var last_scene
 
@@ -19,3 +24,20 @@ func change_scene_to(scene_name):
 	add_child(scenes_loaded[scene_name])
 	if scenes_loaded[scene_name].has_method("on_enter_scene"): scenes_loaded[scene_name].on_enter_scene()
 	last_scene=scenes_loaded[scene_name]
+<<<<<<< Updated upstream
+=======
+	
+	inventario = last_scene.get_node("Inventario")
+	for i in range(espacios.size()):
+		if espacios[i] != "":
+			inventario.AgregarItem(i, contenido[i][0])
+
+func AgarrarItemClickeado(item):
+	for i in range(espacios.size()):
+		if espacios[i] == "":
+			espacios[i] = item.name
+			item.queue_free()
+			contenido[i] = [item.texture_normal, item.texto]
+			inventario.AgregarItem(i, item.texture_normal)
+			return
+>>>>>>> Stashed changes
