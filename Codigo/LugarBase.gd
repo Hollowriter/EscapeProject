8c8df717b91ed30356e672a35ec915a21f089239
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-03-28 18:38:10 -0300

    nuevos textos para entrada de cuartos. Agregados tambien textos por si entras por segunda vez a algunos lugares.

--- Codigo/LugarBase.gd
+++ Codigo/LugarBase.gd
@@ -1,8 +1,17 @@
 extends CanvasLayer
 
+onready var vecesEntradas = 0
 export var textoAlEntrar = ""
+export var textoSegundaVez = ""
 
 onready var cajaDeTexto = $CajaDeTexto
 
 func on_enter_scene():
-	cajaDeTexto.NuevoTexto(textoAlEntrar)
+	SumarVecesEntradas()
+	if vecesEntradas <= 1:
+		cajaDeTexto.NuevoTexto(textoAlEntrar)
+	else:
+		cajaDeTexto.NuevoTexto(textoSegundaVez)
+
+func SumarVecesEntradas():
+	vecesEntradas += 1
